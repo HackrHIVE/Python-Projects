b7d3c684ec94eaea77c149e4f963d455369cddd8
Chirag Rawal <32819177+HackrHIVE@users.noreply.github.com>
2019-07-07 12:00:14 +0530

    Update Geocoder.py

--- Geocoder.py
+++ Geocoder.py
@@ -5,7 +5,7 @@ Before running this script please install googlemaps library
 
 import googlemaps as gm
 
-gmaps_key = gm.Client(key='AIzaSyBh7KtLf7ZRYQfY7curj8BAbZpVHgKXLi8')
+gmaps_key = gm.Client(key='GoogleAPIkey')
 
 address = input('Enter address please : ')
 
